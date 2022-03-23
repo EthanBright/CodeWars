f5f633eabca0e7d962f52e943792e9f1cea95373
Ethan Bright <ethan.bright20@gmail.com>
2022-03-23 19:49:31 -0400

    8 kyu js: 3/23/22
    8 kyu js: 3/23/22- Remove First and Last Character

--- js/main.js
+++ js/main.js
@@ -61,3 +61,10 @@ function repeatStr (n, s) {
   }
  }
  
+ //8 kyu js: 3/23/22- Remove First and Last Character: It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+ function removeChar(str){
+  //You got this!
+  return (str.slice(1,-1));
+ }
+
+ 
