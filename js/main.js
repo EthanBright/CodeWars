601c7a2306752dc4b5685a15279c20db5d98bddd
Ethan Bright <ethan.bright20@gmail.com>
2022-03-22 21:00:59 -0400

    8 kyu js: 3/22/22
    8 kyu js: 3/22/22- String repeat

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-//8 kyu: 3/15/22- Multiply- This code does not execute properly. Try to figure out why.
+//8 kyu js: 3/15/22- Multiply- This code does not execute properly. Try to figure out why.
 function multiply(a, b){
   return(a * b)
 }
 
-//8 kyu: 3/16/22- Opposite number: Very simple, given an integer or a floating-point number, find its opposite.
+//8 kyu js: 3/16/22- Opposite number: Very simple, given an integer or a floating-point number, find its opposite.
 function opposite(number) {
     return -number
   }
 
-//8 kyu: 3/17/22- Even or Odd: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//8 kyu js: 3/17/22- Even or Odd: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 function even_or_odd(number) { 
     if (number % 2 == 0){
       return 'Even';
@@ -17,14 +17,14 @@ function even_or_odd(number) {
     }
   }
 
-//8 kyu: 3/18/22- Convert a Number to a String!: We need a function that can transform a number into a string. What ways of achieving this do you know?
+//8 kyu js: 3/18/22- Convert a Number to a String!: We need a function that can transform a number into a string. What ways of achieving this do you know?
 function numberToString(num) {
   // Return a string of the number here!
   num = num.toString();
   return num
 }
 
-//8 kyu: 3/19/22- Convert boolean values to strings 'Yes' or 'No'.: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//8 kyu js: 3/19/22- Convert boolean values to strings 'Yes' or 'No'.: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 function boolToWord( bool ){
     //...
     if (bool === true){
@@ -34,7 +34,7 @@ function boolToWord( bool ){
     }
   }
 
-//8 kyu: 3/20/22- Reversed Strings: Complete the solution so that it reverses the string passed into it.
+//8 kyu js: 3/20/22- Reversed Strings: Complete the solution so that it reverses the string passed into it.
 function solution(str){
     if (str === "")
         return "";
@@ -42,7 +42,7 @@ function solution(str){
       return solution(str.substr(1)) + str.charAt(0);
   }
 
-//8 kyu: 3/21/22- Return Negative: In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+//8 kyu js: 3/21/22- Return Negative: In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 function makeNegative(num) {
   // Code?
  if (num <= 0) {
@@ -51,3 +51,13 @@ function makeNegative(num) {
    return -Math.abs(num)
  }
 }
+
+//8 kyu js: 3/22/22- String repeat: Write a function called repeatStr which repeats the given string string exactly n times.
+function repeatStr (n, s) {
+  if (n>0){
+    return s.repeat(n);
+  }else{
+    return "";
+  }
+ }
+ 
