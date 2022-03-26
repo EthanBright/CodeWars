c33f9bac2d28f5ecf81f3badc1eeb1543c8a451d
Ethan Bright <ethan.bright20@gmail.com>
2022-03-25 22:07:07 -0400

    8 kyu js: 3/25/22
    8 kyu js: 3/25/22- Beginner Series #1 School Paperwork

--- js/main.js
+++ js/main.js
@@ -71,3 +71,12 @@ function repeatStr (n, s) {
  function noSpace(x){
   return x.split(/\s/).join('');
 }
+
+//8 kyu js: 3/25/22- Beginner Series #1 School Paperwork: Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+  if (n< 0 || m < 0){
+    return 0;
+  }else{
+    return (n*m)
+  }
+}
