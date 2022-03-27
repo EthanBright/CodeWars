0edc971a13f90f6f4d45d9ce61aa73283b4594e4
Ethan Bright <ethan.bright20@gmail.com>
2022-03-27 11:01:50 -0400

    8 kyu js: 3/27/22

--- js/main.js
+++ js/main.js
@@ -86,3 +86,13 @@ function past(h, m, s){
   //#Happy Coding! ^_^
   return ((h*3600000)+(m*60000)+(s*1000));
 }
+
+//8 kyu js: 3/27/22- Grasshopper - Summation: Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+function summation(num) {
+  // Code here
+  let sum = 0;
+  for(let i = 0; i <= num; i++){
+    sum += i;
+  }
+  return sum;
+}
