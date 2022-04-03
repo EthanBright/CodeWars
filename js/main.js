2fe83f36652cb26cdbf376864ee88b61c86c9561
Ethan Bright <ethan.bright20@gmail.com>
2022-04-03 12:44:24 -0400

    8 kyu js: 4/3/22

--- js/main.js
+++ js/main.js
@@ -128,3 +128,36 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return (i * 2);
 }
+
+//8 kyu js: 4/3/22- Get Planet Name By ID: The function is not returning the correct values. Can you figure out why?
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+      break;
+    case 2:
+      name = 'Venus'
+      break;
+    case 3:
+      name = 'Earth'
+      break;
+    case 4:
+      name = 'Mars'
+      break;
+    case 5:
+      name = 'Jupiter'
+      break;
+    case 6:
+      name = 'Saturn'
+      break;
+    case 7:
+      name = 'Uranus'
+      break;
+    case 8:
+      name = 'Neptune'
+      break;
+  }
+  
+  return name;
+}
