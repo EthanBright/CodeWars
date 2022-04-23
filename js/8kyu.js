527d8170ae1c9be68d37b43de8da55fb6cf25fa4
Ethan Bright <ethan.bright20@gmail.com>
2022-04-23 14:37:30 -0400

    8 kyu js: 4/23/22

--- js/8kyu.js
+++ js/8kyu.js
@@ -297,3 +297,12 @@ function switchItUp(number){
         break;
   }
 }
+
+//8 kyu js: 4/23/22- Keep up the hoop
+function hoopCount (n) {
+  if (n >= 10){
+    return 'Great, now move on to tricks'
+  }else {
+    return 'Keep at it until you get it'
+  }
+}
