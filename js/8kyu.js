d1584da88c2ca581d74ad8a02053347c071cec71
Ethan Bright <ethan.bright20@gmail.com>
2022-06-19 20:25:26 -0400

    8 kyu js: 6/19/22

--- js/8kyu.js
+++ js/8kyu.js
@@ -828,3 +828,16 @@ function combineNames(first, last){
   let fullName = first.concat(' ',last);
   return fullName
 }
+
+//8 kyu js: 6/19/22- Age Range Compatibility Equation
+function datingRange(age){
+  if (age <= 14){
+  let min = Math.floor(age - 0.10 * age);
+  let max = Math.floor(age + 0.10 * age);
+  return `${min}-${max}`;
+  }else{
+  let min = Math.floor(age/2 + 7);
+  let max = Math.floor((age - 7) * 2);
+  return `${min}-${max}`;
+  }
+}
