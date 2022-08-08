39d66b7a865900393c28a89dd58d2f5c2716f305
Ethan Bright <ethan.bright20@gmail.com>
2022-08-07 21:18:47 -0400

    8 kyu js: 8/7/22

--- js/8kyu.js
+++ js/8kyu.js
@@ -1210,3 +1210,14 @@ function stringToArray(string){
     let string = str.split(letter);
     return string.length - 1;
   }
+
+  //8 kyu js: 8/7/22- Fix your code before the garden dies!
+  function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${40- mm}mm of water`;
+    }else{
+         return "Your plant has had more than enough water for today!"
+    }
+  }
+
+  
